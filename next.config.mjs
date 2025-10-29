f38e8cee821f77ff0146f7128d2463c84acfd902
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-10-29 16:58:12 +0100

    Updated lighthouse workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export', // This replaces `next export`
+  // other settings here
 };
 
-export default nextConfig;
+module.exports = nextConfig;
