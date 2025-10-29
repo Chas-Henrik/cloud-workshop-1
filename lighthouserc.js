69fd7752d7d688d9f8a0a5da0a74626fc6a9837f
Henrik Suurik <henrik.suurik@chasacademy.se>
2025-10-29 16:41:09 +0100

    Updated lighthouse CI

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,12 @@
+// .lighthouserc.js
 module.exports = {
   ci: {
+    collect: {
+      staticDistDir: "out", // or ".next" if you're serving it differently
+      startServerCommand: "npm start", // optional, for SSR
+    },
     upload: {
-      target: 'temporary-public-storage',
+      target: "temporary-public-storage", // or 'lhci' server if you host one
     },
   },
 };
